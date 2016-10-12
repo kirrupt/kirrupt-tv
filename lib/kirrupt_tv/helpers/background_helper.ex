19ec65be2a45f497e1952ef7838b21d8e1b35342
Dejan Levec <dejan@dejanlevec.com>
2016-10-12 20:37:16 +0200

    Enum.random throws exception when array is empty

--- lib/kirrupt_tv/helpers/background_helper.ex
+++ lib/kirrupt_tv/helpers/background_helper.ex
@@ -1,9 +1,10 @@
 defmodule KirruptTv.Helpers.BackgroundHelpers do
-  
+
   def random_background(shows) do
     shows
     |> Enum.map(fn(s) -> s.fixed_background end)
     |> Enum.reject(fn(bg) -> bg == nil end)
-    |> Enum.random
+    |> Enum.shuffle
+    |> Enum.take(1)
   end
 end
