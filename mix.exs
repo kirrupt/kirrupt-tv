87d7da216c6829f9bdc7ecbfee83abd2313bc860
Dejan Levec <dejan@dejanlevec.com>
2017-12-23 12:06:29 +0100

    update libs

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule KirruptTv.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.3.0"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:mariaex, "~> 0.8.2"},
